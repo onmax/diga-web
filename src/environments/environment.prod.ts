54fb0a6807272fbc02095603a1b15b116c85f27d
Max <maximogarciamtnez@gmail.com>
2018-09-23 09:07:11 +0200

    fixed problem and added ingo to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  split: '*!*',
+  spreadsheets: {
+    teachers: '1jE75JDa39O0ZWu-91fuzF3CFQhtZ1Kv6jzDK_p42apg',
+    grade: {
+      subjects: '1rgMuzgnO4ruJTSzJgKnhjP_L3S6WKrh9DQFB83tDy5k'
+    },
+    posgrade: {
+      subjects: '12sfy79wCfcfQpOTYxw6d17Ewqe7IlSL3RWKpVDVoVHk'
+    },
+    reports: '1rqE_8CLNvzlDQgI5sJacjzqB2-jyVzwrUbJlpIFzYc0'
+  }
 };
